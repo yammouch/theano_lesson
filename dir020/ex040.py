5f50b2e0c034639f1da9e996d76897b44273dc24
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-07-20 17:25:27 +0900

    First checkin.

---
+++ dir020/ex040.py
@@ -0,0 +1,16 @@
+import theano.tensor as T
+from theano import shared, function
+
+state = shared(0)
+inc = T.iscalar('inc')
+accumulator = function([inc], state, updates=[(state, state+inc)])
+
+print state.get_value()
+print accumulator(1)
+print state.get_value()
+print accumulator(300)
+print state.get_value()
+
+state.set_value(-1)
+print accumulator(3)
+print state.get_value()
