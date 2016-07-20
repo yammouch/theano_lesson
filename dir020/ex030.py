b1f76a155451d250ec9a059ae4948f6e97aa04e8
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-07-20 17:16:12 +0900

    First checkin.

--- dir020/ex030.py
+++ dir020/ex030.py
@@ -1,9 +1,18 @@
-import theano
+import theano.tensor as T
 from theano import In
 from theano import function
 
-x, y = theano.tensor.dscalars('x', 'y')
+x, y = T.dscalars('x', 'y')
 z = x + y
 f = function([x, In(y, value=1)], z)
 print f(33)
 print f(33, 2)
+
+x, y, w = T.dscalars('x', 'y', 'w')
+z = (x + y) * w
+f = function([x, In(y, value=1), In(w, value=2, name='w_by_name')], z)
+print f(33)
+print f(33, 2)
+print f(33, 0, 1)
+print f(33, w_by_name=1)
+print f(33, w_by_name=1, y=0)
